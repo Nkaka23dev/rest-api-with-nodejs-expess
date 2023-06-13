349a2d79e00b1e8693008ec7edc9371021a7a8ff
Nkaka23dev <nkakaeric96@gmail.com>
2023-06-13 18:06:17 +0300

    feat: done with express

--- app.js
+++ app.js
@@ -1,40 +1,21 @@
-const http = require('http');
-const fs = require('fs')
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
+const app = express();
 
-    let path = './views/';
+app.listen(8000);
 
-    switch (req.url) {
-        case '/':
-            path += 'index.html'
-            res.statusCode = 200;
-            break;
-        case '/about':
-            path += 'about.html'
-            res.statusCode = 200;
-            break;
-        case '/about-us':
-            path += '';
-            res.statusCode = 301;
-            res.setHeader('Location', '/about');
-            break;
-        default:
-            res.statusCode = 404;
-            path += "404.html";
-            break
-    }
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', { root: __dirname });
+});
 
-    fs.readFile(path, (error, data) => {
-        if (error) {
-            console.log(error, "Error occured")
-        } else {
-            res.write(data);
-            res.end();
-        }
-    })
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname })
+});
+
+app.get('/about-us', (req, res) => {
+    res.redirect('/about')
 })
-server.listen(8000, "LocalHost", () => {
-    console.log("Connection successfully created on port 3000")
+
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname })
 })
